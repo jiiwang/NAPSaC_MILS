e3770f15366bd635d9b265998ceba29a28cb9a26
Ji Wang <hytonwons@Jis-Mac-Studio.local>
2024-07-23 02:13:16 -0700

    model 2 updated...

--- fixed_point/test/test_noisy_mul.m
+++ fixed_point/test/test_noisy_mul.m
@@ -1,51 +1,53 @@
+addpath("src/nbit_fi/")
+addpath("src/util/")
+
+% random seed, for reproducible results
+rng("twister");
+
 % # of trials per run
-ntrial = 100;
+ntrial = 200;
 % # of runs
 n = 500;
-x = zeros(n,2);
-y = zeros(n,1);
-y1 = zeros(n,ntrial);
-y2 = zeros(n,ntrial);
+y = zeros(n,ntrial);
 
-nbit = 8;
+nbit = 16;
 T = numerictype(1,nbit+1,nbit);
-
-round4 = @(x) round(x.*10.^4)./10.^4;
 
 for i = 1:n
     i
-    a = round4(rand(1));
-    b = round4(rand(1));
+    a = rand(1);
+    b = rand(1);
 
-    c_8 = fi_mul(a,b,T);
-    y(i) = c_8;
+    c_fi = fi_mul(a,b,T);
 
     for j = 1:ntrial
 
-        c_8_no = noisy_mul(a, b, nbit, nbit, 1);
+        c_fi_no = noisy_mul(a, b, nbit, nbit, 1);
         
-        err1 = c_8.double-c_8_no.double;
+        err = c_fi.double-c_fi_no.double;
                 
-        y1(i, j) = err1;
+        y(i, j) = err;
     end
     
 end
 
-y1 = mean(y1,2);
+y = mean(y,2);
 
 figure;
 set(gcf,'position',[300,300,1600,800]);
-histogram(y1);
-xline(2^-8, '-r', 'LineWidth', 1.5);
-xline(2^-7, 'LineWidth', 1.5);
-leg1 = legend('$c_{8}-\widehat{c_{8}}$','$2^{-8}$', '$2^{-7}$');
-set(leg1,'Interpreter','latex');
-set(leg1,'FontSize',30);
-set(leg1, 'Location','northwest');
+histogram(y, 15);
+xline(2^-16, '-r', 'LineWidth', 1.5);
+% xline(2^-7, 'LineWidth', 1.5);
+leg = legend('$c_{16}-\widehat{c_{16}}$','$2^{-16}$');
+set(leg,'Interpreter','latex');
+set(leg,'FontSize',30);
+set(leg, 'Location','northwest');
 % 'Orientation','horizontal');
-ylim([0,n/5]);
+ylim([0,120]);
 xlabel('error')
 ylabel('count')
 ax = gca; 
 ax.FontSize = 30; 
 box on;
+
+exportgraphics(gcf, '16_vs_16no_mul_err_count.pdf', 'ContentType', 'vector');
